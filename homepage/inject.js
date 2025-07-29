3bb3b8bd2ce305af4831e7f120aee6b8bd04d086
AK <46279060+U061@users.noreply.github.com>
2025-07-29 15:42:41 +0100

    Update inject.js

--- homepage/inject.js
+++ homepage/inject.js
@@ -302,6 +302,37 @@ function waitForElm(selector) {
     });
 }
 
+class ButtonSwitch {
+  constructor(domNode) {
+    this.switchNode = domNode;
+    this.switchNode.addEventListener('click', () => this.toggleStatus());
+
+    // Set background color for the SVG container Rect
+    var color = getComputedStyle(this.switchNode).getPropertyValue(
+      'background-color'
+    );
+    var containerNode = this.switchNode.querySelector('rect.container');
+    containerNode.setAttribute('fill', color);
+  }
+
+  // Switch state of a switch
+  toggleStatus() {
+    const currentState =
+      this.switchNode.getAttribute('aria-checked') === 'true';
+    const newState = String(!currentState);
+
+    this.switchNode.setAttribute('aria-checked', newState);
+  }
+}
+
+// Initialize switches
+window.addEventListener('load', function () {
+  // Initialize the Switch component on all matching DOM nodes
+  Array.from(document.querySelectorAll('button[role^=switch]')).forEach(
+    (element) => new ButtonSwitch(element)
+  );
+});
+
 
 /*var fac= {
   x1122: "Cavendish",
